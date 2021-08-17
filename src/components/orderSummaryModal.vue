d46736fa41e2d766da6ae285a6af18f2e3478e80
tzyn (windows) <tzyn.wang@gmail.com>
2021-08-17 23:46:59 +0800

    feat: add bg-color for summary modal

--- src/components/orderSummaryModal.vue
+++ src/components/orderSummaryModal.vue
@@ -1,18 +1,20 @@
 <template>
-  <div :class="['order-summary', { 'd-none': !getFormState }]">
-    <span class="order-summary-close" @click="mutateFormState(false)"></span>
-    <div>title: {{ getFormInput.title }}</div>
-    <div>name: {{ getFormInput.name }}</div>
-    <div>tel: {{ getFormInput.tel }}</div>
-    <div>email: {{ getFormInput.email }}</div>
-    <div>city: {{ getFormInput.city }}</div>
-    <div>address: {{ getFormInput.address }}</div>
-    <div>cardHolderName: {{ getFormInput.cardHolderName }}</div>
-    <div>cardNumber: {{ getFormInput.cardNumber }}</div>
-    <div>cardExpiredDate: {{ getFormInput.cardExpiredDate }}</div>
-    <div>cardCvc: {{ getFormInput.cardCvc }}</div>
-    <div>deliveryFee: {{ getSelectedDeliveryFee }}</div>
-    <div>orderSum: {{ getOrderSum }}</div>
+  <div :class="['modal-container', { 'd-none': !getFormState }]">
+    <div class="order-summary">
+      <span class="order-summary-close" @click="mutateFormState(false)"></span>
+      <div>title: {{ getFormInput.title }}</div>
+      <div>name: {{ getFormInput.name }}</div>
+      <div>tel: {{ getFormInput.tel }}</div>
+      <div>email: {{ getFormInput.email }}</div>
+      <div>city: {{ getFormInput.city }}</div>
+      <div>address: {{ getFormInput.address }}</div>
+      <div>cardHolderName: {{ getFormInput.cardHolderName }}</div>
+      <div>cardNumber: {{ getFormInput.cardNumber }}</div>
+      <div>cardExpiredDate: {{ getFormInput.cardExpiredDate }}</div>
+      <div>cardCvc: {{ getFormInput.cardCvc }}</div>
+      <div>deliveryFee: {{ getSelectedDeliveryFee }}</div>
+      <div>orderSum: {{ getOrderSum }}</div>
+    </div>
   </div>
 </template>
 
