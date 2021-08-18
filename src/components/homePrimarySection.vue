479e2b75f9872395d3218ac777cf271c04e27b6a
tzyn (windows) <tzyn.wang@gmail.com>
2021-08-18 18:35:26 +0800

    feat: add homepage primary section

---
+++ src/components/homePrimarySection.vue
@@ -0,0 +1,101 @@
+<template>
+  <section class="primary">
+    <div class="primary__container--main">
+      <img
+        class="primary__product-photo"
+        :src="require('@/assets/images/primary-main.jpg')"
+        alt="primary section product photo"
+      />
+    </div>
+    <div class="primary__container--secondary">
+      <img
+        class="primary__product-photo"
+        :src="require('@/assets/images/primary-secondary.jpg')"
+        alt="primary section product photo"
+      />
+    </div>
+    <div class="primary__container--sub">
+      <img
+        class="primary__product-photo"
+        :src="require('@/assets/images/primary-sub-1.jpg')"
+        alt="primary section product photo"
+      />
+    </div>
+    <div class="primary__container--sub">
+      <img
+        class="primary__product-photo"
+        :src="require('@/assets/images/primary-sub-2.jpg')"
+        alt="primary section product photo"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "homePrimarySection",
+};
+</script>
+
+<style lang="scss" scoped>
+.primary {
+  padding: 8vh 0; // 80vh * 10%
+  margin-top: -16vh;
+
+  [class^="primary__container"] {
+    overflow: hidden;
+  }
+
+  &__container--main {
+    height: 90vh;
+  }
+
+  &__container--secondary {
+    height: 60vh;
+  }
+
+  &__container--sub {
+    height: 33vh;
+  }
+
+  &__product-photo {
+    width: 100%;
+    height: 100%;
+    display: block;
+    object-fit: cover;
+    object-position: center top;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .primary {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 40vh);
+    grid-template-areas:
+      "primary primary secondary secondary"
+      "primary primary sub-1 sub-2";
+
+    [class^="primary__container"] {
+      height: 100%;
+    }
+
+    &__container--main {
+      grid-area: primary;
+    }
+
+    &__container--secondary {
+      grid-area: secondary;
+    }
+
+    &__container--sub {
+      &:first-child {
+        grid-area: sub-1;
+      }
+      &:last-child {
+        grid-area: sub-2;
+      }
+    }
+  }
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,16 +2,19 @@
   <main class="container">
     <!-- <h1 class="title">首頁 (´・ω・`)</h1> -->
     <homeLanding />
+    <homePrimarySection />
   </main>
 </template>
 
 <script>
 import homeLanding from "./../components/homeLanding.vue"
+import homePrimarySection from "./../components/homePrimarySection.vue"
 
 export default {
   name: "home",
   components: {
-    homeLanding
+    homeLanding,
+    homePrimarySection
   }
 }
 </script>
